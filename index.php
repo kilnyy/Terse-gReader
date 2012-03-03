f9a44668c83819e372ff43bdb43f03e4db9daaaf
Xu Mingzhao <kilnyy@gmail.com>
2012-03-03 15:50:11 +0800

    try to fixed width

--- index.php
+++ index.php
@@ -24,7 +24,8 @@
 ?>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8; width=device-width"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>gReader</title>
         <link rel=stylesheet href="style.css" type="text/css" /> 
     </head>
