6608e0d9b5adf1c133316dbd120ef3b25062d29b
Xu Mingzhao <kilnyy@gmail.com>
2012-03-03 16:44:03 +0800

    fixed phone read config

--- gReader.py
+++ gReader.py
@@ -29,16 +29,17 @@ w3atom = '{http://www.w3.org/2005/Atom}'
 tree = etree.fromstring(s)
 all_entrys = tree.findall(w3atom + 'entry')
 for entry in all_entrys:
-    result += '<div class = "entry rb">\n'
+    result += '<div class="entry rb">\n'
     all_titles = entry.findall(w3atom + 'title')
     for title in all_titles:
         result += "<h3>\n" + title.text.encode('utf-8') + "</h3>\n"
     all_content = entry.findall(w3atom + 'content')
     for content in all_content:
-        result += "<div>\n" + content.text.encode('utf-8') + "</div>\n"
+        result += '<div class="content">\n' + content.text.encode('utf-8') + "</div>\n"
     all_summary = entry.findall(w3atom + 'summary')
     for summary in all_summary:
-        result += "<div>\n" + summary.text.encode('utf-8') + "</div>\n"
+        result += '<div class="content">\n' + summary.text.encode('utf-8') + "</div>\n"
     result += '</div>\n'
-result = re.sub('\<img[^>]*>', '', result) 
+result = re.sub('<img[^>]*>', '', result) 
+result = re.sub('<iframe.*>.*<\/iframe>', '', result)
 print result
