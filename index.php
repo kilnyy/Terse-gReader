8185dbe8a24768aadb0a7cb9148fa2e169d99bea
Xu Mingzhao <kilnyy@gmail.com>
2012-03-03 23:41:59 +0800

    fixed icon bug

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="shortcut icon" href="favicon.ico" title="Favicon">
         <title>gReader</title>
         <link rel=stylesheet href="style.css" type="text/css" /> 
     </head>
